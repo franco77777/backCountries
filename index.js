f0844e9cf5e0b066d4ad6e3d351896c5f4b3133d
franco7777777 <francoaresis@gmail.com>
2022-12-21 18:18:16 -0300

    asdf

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const { getCountries } = require("./src/controllers/controlcountry.js")
 const port = process.env.PORT || 3977
 
 //conn.sync({ force: false }).then(() => {
-server.listen(port, async () => {
-  //await getCountries()
-  console.log("%s listening at " + port) // eslint-disable-line no-console
-})
+server.listen(
+  port,
+  /* async */ () => {
+    //await getCountries()
+    console.log("%s listening at " + port) // eslint-disable-line no-console
+  }
+)
