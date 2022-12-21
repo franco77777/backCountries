6c9e462f1a70b0696f563823abfd1f1e18707596
franco7777777 <francoaresis@gmail.com>
2022-12-21 18:25:51 -0300

    3

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const server = require("./src/app.js")
 const { conn } = require("./src/db.js")
 const { getCountries } = require("./src/controllers/controlcountry.js")
-
+const express = require("express")
+const routes = require("./src/routes/index")
 const port = process.env.PORT || 3977
+
+const server = express()
+
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
+server.use("/", routes)
 
 //conn.sync({ force: false }).then(() => {
 server.listen(
