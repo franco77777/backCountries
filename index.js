007d356c66113aa3de52ebcdbbac0b0bcccf31fc
franco7777777 <francoaresis@gmail.com>
2022-12-21 17:31:29 -0300

    asd

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const server = require("./src/app.js")
 const { conn } = require("./src/db.js")
 const { getCountries } = require("./src/controllers/controlcountry.js")
 
+const PORT = process.env.PORT || 3977
+
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(PORT, async () => {
     await getCountries()
-    console.log("%s listening at 3001") // eslint-disable-line no-console
+    console.log("%s listening at " + PORT) // eslint-disable-line no-console
   })
 })
--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ const cookieParser = require("cookie-parser")
 const morgan = require("morgan")
 const cors = require("cors")
 const routes = require("./routes/index.js")
+const bodyParse = require("body-parser")
 
 require("./db.js")
 
 const server = express()
 
+
+
 server.name = "API"
-server.use(cors())
-server.use(express.urlencoded({ extended: true }))
-server.use(express.json())
+//server.use(cors())
+//server.use(express.urlencoded({ extended: true }))
+//server.use(express.json())
+server.use(bodyParse.json())
+server.use(bodyParse.urlencoded({extended:true}))
 
 server.use("/", routes)
 
