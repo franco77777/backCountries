63538267f809e62ecc94c665c4657078113abcd9
franco7777777 <francoaresis@gmail.com>
2022-12-21 17:43:42 -0300

    2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const server = require("./src/app.js")
 const { conn } = require("./src/db.js")
 const { getCountries } = require("./src/controllers/controlcountry.js")
 
-const PORT = process.env.PORT || 3977
+const port = process.env.PORT || 3977
 
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, async () => {
+conn.sync({ force: false }).then(() => {
+  server.listen(port, async () => {
     await getCountries()
-    console.log("%s listening at " + PORT) // eslint-disable-line no-console
+    console.log("%s listening at " + port) // eslint-disable-line no-console
   })
 })
